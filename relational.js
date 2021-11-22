f174a125e8d508da76d298b5a69326bb46bd618c
turvec <okaforturvec@gmail.com>
2021-11-22 12:10:21 +0100

    commit

--- relational.js
+++ relational.js
@@ -1,6 +1,7 @@
 mixshopping = ["pen", "ball", 10, "watch",false];
 mixshopping.push("paint");
 console.log(mixshopping);
+console.log(mixshopping);
 
 int = [1,2,3];
 answer= (int[0] + int[2])/int[1];
