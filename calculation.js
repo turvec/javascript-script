0a66619f3cc22b92d05697bca4acdfac571e8bcf
turvec <okaforturvec@gmail.com>
2021-11-23 13:20:35 +0100

    commit

---
+++ calculation.js
@@ -0,0 +1,16 @@
+numbers=[6,3,4,2,5,1];
+
+answers=(numbers[0]*numbers[2])/numbers[3];
+console.log(answers);
+
+// r = prompt("input the radius");
+// const pie = 3.14;
+// Area = pie*r**2;
+// alert(Area);
+
+firstName = prompt("enter name");
+jamb = prompt("jamb score");
+postUtme =prompt("postutme score");
+
+total = (jamb/8)+postUtme;
+alert("Dear "+ firstName +" your total score is " + total  );
