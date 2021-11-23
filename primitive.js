2a45e15a10e90802f7aed7d793d15afcca802198
turvec <okaforturvec@gmail.com>
2021-11-23 12:28:34 +0100

    commit

--- primitive.js
+++ primitive.js
@@ -9,5 +9,7 @@
 text = " 18 is still a string as long as it's in a quotation mark";
 numeric = 2783787;
 booLean = false;
-unDefined
+hello
 empty = null;
+
+console.log(booLean);
--- reference.js
+++ reference.js
@@ -1,11 +1,30 @@
-mixshopping = ["pen", "ball", 10, "watch",false];
-mixshopping.push("paint");
-console.log(mixshopping[0]);
-console.log(mixshopping[1]);
-console.log(mixshopping[2]);
-console.log(mixshopping[3]);
-console.log(mixshopping);
+// mixshopping = ["pen", "ball", 10, "watch",false];
+// mixshopping.push("paint");
+// console.log(mixshopping[0]);
+// console.log(mixshopping[1]);
+// console.log(mixshopping[2]);
+// console.log(mixshopping[3]);
+// console.log(mixshopping);
 
-int = [1,2,3];
-answer= (int[0] + int[2])/int[1];
-console.log(answer);
+// int = [1,2,3];
+// answer= (int[0] + int[2])/int[1];
+// console.log(answer);
+
+array = ['phone', 'ball', 12, true,];
+console.log(array[3]);
+
+car = {
+    color : "red",
+    brand : "benz",
+    engine : "500hp",
+    tire : true,
+};
+
+turvec = {
+    school: "uniport",
+    depertment : "enginering",
+    course: ["math", "chem","phy"]
+};
+console.log(turvec.course[2]);
+
+console.log(car.brand);
